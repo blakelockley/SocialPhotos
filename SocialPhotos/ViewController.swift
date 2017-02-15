af32c2dbbecaf1f187e8e66571e7bf954251654d
Blake Lockley <bwl@blakes-macbook-pro.local>
2017-02-15 12:50:31 +1100

    Initial Commit

---
+++ SocialPhotos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialPhotos
+//
+//  Created by Blake Lockley on 15/02/2017.
+//  Copyright © 2017 Blake Lockley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
